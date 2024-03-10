625eedcab2490bbe383d0a193a18e16637e3544e
binst090104 <bin1st090104@gmail.com>
2024-03-10 19:27:14 +0700

    add compile.sh file

---
+++ compile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SOURCE="source"
+BUILD="build"
+COMPILER_CALL="gcc -std=c11"
+C_OBJECTS=""
+
+for C_SOURCE in $(find ${SOURCE} -name "*.c")
+do
+    # echo ${C_SOURCE}
+    # echo $(basename ${C_SOURCE} .c)
+    BASE_NAME=$(basename ${C_SOURCE} .c)
+    # echo ${BASE_NAME}
+    ${COMPILER_CALL} -c ${C_SOURCE} -o ${BUILD}/${BASE_NAME}.o
+    C_OBJECTS="${C_OBJECTS}${BUILD}/${BASE_NAME}.o "
+done
+
+# echo ${C_OBJECTS}
+
+${COMPILER_CALL} ${C_OBJECTS} -o main
+
+./main.exe
+
+rm -rf build/*
