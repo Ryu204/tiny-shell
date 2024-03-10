db8ee405563775801dbd7b83a83ae72c9e2bcf16
NguyenAnhBao <baotrass@gmail.com>
2024-03-10 19:15:43 +0700

    fix compilation errors on `gcc`

---
+++ source/runner/help.c
@@ -0,0 +1,9 @@
+#include "help.h"
+#include "../core/io_wrap.h"
+
+enum run_result run_help() {
+    format_output("%s",
+                  "\"help\": print this message\n"
+                  "\"exit\": exit shell\n");
+    return RUN_OK;
+}
