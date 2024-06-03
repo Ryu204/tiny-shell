df17c539cb322cf3b31515d727e35f83b1cbf114
bin1st090104 <binst090104@gmail.com>
2024-06-03 10:15:27 +0700

    handle CTRL+C

--- source/runner/launch_executable.c
+++ source/runner/launch_executable.c
@@ -1,6 +1,18 @@
 #include "launch_executable.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+void handler(int sig) {
+    if (sig == SIGINT) {
+        printf("Dectect SIGINT\n");
+    }
+}
 
 enum run_result run_launch_executable(const struct args args) {
+    if (!args.background) {
+        signal(SIGINT, handler);
+    }
     bool result = launch_executable(args);
     return result ? RUN_OK : RUN_FAILED;
 }
