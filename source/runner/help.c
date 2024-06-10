018e8afd08cbb89b127d7cfc2e8dd0e6f5019894
Hachi <hachit1k29@gmail.com>
2024-06-10 15:57:25 +0700

    add some commands into help

--- source/runner/help.c
+++ source/runner/help.c
@@ -5,6 +5,12 @@ enum run_result run_help() {
     format_output("%s",
                   "\"help\":    print this message\n"
                   "\"cd\":      change directory\n"
-                  "\"exit\":    exit shell\n");
+                  "\"exit\":    exit shell\n"
+                  "\"time\":    get current time\n"
+                  "\"date\":    get current date\n"
+                  "\"list\":    get list of running processes\n"
+                  "\"stop\":    stop a process\n"
+                  
+                  );
     return RUN_OK;
 }
