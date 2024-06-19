667b8e8480eab69a1a6b609569733e8a89ee9f38
Ryu204 <nguyenanhbao2356@gmail.com>
2024-06-20 01:13:00 +0700

    fix linter error

--- source/os/operations_win.c
+++ source/os/operations_win.c
@@ -143,10 +143,12 @@ bool lsdir(const os_char *dir) {
     LARGE_INTEGER fileSize;
     int countFile = 0;
 
-    os_char *combined = (os_char *)malloc((strlen(dir) + 3) * sizeof(os_char));
+    unsigned int dir_len = strlen(dir);
+    os_char *combined = (os_char *)malloc((dir_len + 3) * sizeof(os_char));
 
-    strcpy(combined, dir);
-    strcpy(combined + strlen(dir), "/*");
+    memcpy(combined, dir, dir_len);
+    memcpy(combined + dir_len, "/*", 2);
+    combined[dir_len + 2] = '\0';
 
     HANDLE hFind = FindFirstFile(combined, &data);
 
@@ -377,7 +379,7 @@ bool enum_proc() {
     pe.dwSize = sizeof(PROCESSENTRY32);
     Process32First(hSnapshot, &pe);
     do {
-        printf("PID: %6u PPID: %6u T: %3u Name: %s \n", pe.th32ProcessID, pe.th32ParentProcessID, pe.cntThreads, pe.szExeFile);
+        printf("PID: %6lu PPID: %6lu T: %3lu Name: %s \n", pe.th32ProcessID, pe.th32ParentProcessID, pe.cntThreads, pe.szExeFile);
     } while(Process32Next(hSnapshot, &pe));
 
     CloseHandle(hSnapshot);
